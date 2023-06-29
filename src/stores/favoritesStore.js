eb94b1c23b6b8cdf55e8be79261b362b6a59f1d9
Chaneice Jackson <c.jackson0126@gmail.com>
2023-06-28 20:32:20 -0500

    deployment

--- src/stores/favoritesStore.js
+++ src/stores/favoritesStore.js
@@ -4,18 +4,16 @@ const localStorageFavorites = JSON.parse(localStorage.getItem('recipes'))
 
 export const favorites = ref(localStorageFavorites || [])
 
-const saveRecipes = () => {
-  localStorage.setItem('recipes', JSON.stringify(favorites.value))
-}
-
 const saveFavorites = () => {
   localStorage.setItem('recipes', JSON.stringify(favorites.value))
 }
 
 export const addToFavorites = (recipe) => {
   favorites.value = [...favorites.value, recipe]
+  saveFavorites()
 }
 
 export const removeFavorite = (id) => {
   favorites.value = favorites.value.filter((r) => r.recipeId !== id)
+  saveFavorites()
 }
